6d312757841a80073b49b21a1abdafe2e3c5279d
篮球不止是个球 <351223441@qq.com>
2018-03-22 12:23:19 +0800

    Site updated: 2018-03-22 12:23:19

--- js/index.js
+++ js/index.js
@@ -32,3 +32,33 @@ window.onscroll = function () {
     }
   }
 }
+
+// 添加复制文章内容自动增加作者信息
+function addLink() {
+    var body_element = document.getElementsByTagName('body')[0];
+    var selection;
+
+    if( window.getSelection ) {//DOM,FF,Webkit,Chrome,IE10
+        selection = window.getSelection();
+    } else if( document.getSelection ) {//IE10
+        selection= document.getSelection();
+
+    } else if( document.selection ) {//IE6+10-
+        selection= document.selection.createRange().text;
+    } else {
+        selection= "";
+    }
+
+    var pagelink = "<br /><br /> 作者：小bbbbbb<br />链接： <a href='"+document.location.href+"'>"+document.location.href+"</a><br />来源：佛性前端<br />生活不止眼前的枸杞。";
+    var copy_text = selection + pagelink;
+    var new_div = document.createElement('div');
+    new_div.style.left = '-99999px';
+    new_div.style.position = 'absolute';
+    body_element.appendChild( new_div );
+    new_div.innerHTML = copy_text ;
+    selection.selectAllChildren( new_div );
+    window.setTimeout(function() {
+        body_element.removeChild( new_div );
+    }, 0 );
+}
+document.body.oncopy = addLink;
